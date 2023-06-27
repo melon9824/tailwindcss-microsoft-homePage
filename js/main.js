974b3fb90025111beb299271b987ff4b8f6fa6b2
khorshed-alam <ka.melon1968@gmail.com>
2023-06-28 00:19:46 +0600

    scroll-up button added

--- js/main.js
+++ js/main.js
@@ -24,3 +24,15 @@ moreBtn.addEventListener('click', () => {
 
     more.classList.toggle('toggleStyle');
 })
+
+
+/**
+ * ! show scroll up
+ */
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 150 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
